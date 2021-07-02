2ca3438f85a1f9fd860279fe2131e7145ac1376d
Deepak Yadav <dk02121998@gmail.com>
2021-07-02 18:19:20 +0530

    adding comment

--- app/src/main/java/com/techwithcode/uniconvert/MainActivity.java
+++ app/src/main/java/com/techwithcode/uniconvert/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     private Button button;
     private EditText editText;
 
+    // Github Testing
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
